6be8ce3c070b4e4efa1b1902fa146c61999e05be
Dreamer <dreamerlee9489@outlook.com>
2022-05-30 17:24:15 +0800

    Update

---
+++ Assets/Scripts/UI/Panel/PausePanel.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using App.Manager;
+
+namespace App.UI
+{
+	public class PausePanel : BasePanel
+	{
+		Button btnClose = null;
+		Button btnSetting = null;
+		Button btnSave = null;
+		Button btnQuit = null;
+
+		void Awake()
+		{
+			btnClose = transform.GetChild(0).GetComponent<Button>();
+			btnSetting = transform.GetChild(1).GetComponent<Button>();
+			btnSave = transform.GetChild(2).GetComponent<Button>();
+			btnQuit = transform.GetChild(3).GetComponent<Button>();
+			btnClose.onClick.AddListener(() => {
+				gameObject.SetActive(false);
+			});
+			btnSetting.onClick.AddListener(() => {
+
+			});
+			btnSave.onClick.AddListener(() => {
+				GameManager.Instance.onSavingData();
+				gameObject.SetActive(false);
+			});
+			btnQuit.onClick.AddListener(() => {
+				Application.Quit();
+			});
+		}
+	}
+}
